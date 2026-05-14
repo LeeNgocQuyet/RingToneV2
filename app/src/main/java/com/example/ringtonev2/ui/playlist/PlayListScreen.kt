a4b5d17a6fd080ab8a72f715f981a98999295ba9
LeeNgocQuyet <159886685+LeeNgocQuyet@users.noreply.github.com>
2026-05-14 14:26:39 +0700

    feat: implement PlaylistScreen with Downloads and Favorites tabs
    - Add `PlaylistState` and `PlaylistTab` to manage UI state.
    - Implement `PlaylistScreenViewModel` using Hilt for managing ringtone downloads and favorites.
    - Update `PlaylistScreen` UI with `LazyColumn`, tab switching, and empty state handling.
    - Rename and integrate `PlaylistScreen` into the main navigation flow.

--- app/src/main/java/com/example/ringtonev2/ui/playlist/PlayListScreen.kt
+++ app/src/main/java/com/example/ringtonev2/ui/playlist/PlayListScreen.kt
@@ -1,7 +1,313 @@
 package com.example.ringtonev2.ui.playlist
 
-fun PlayListScreen(
-    onOpenPlayer: (String) -> Unit
-){
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.ringtonev2.R
+import com.example.ringtonev2.components.RingtoneItemRow
+import com.example.ringtonev2.data.mapper.toAudioPreview
+import com.example.ringtonev2.data.mapper.toRingtone
+import com.example.ringtonev2.ui.theme.AppTypography
+import com.example.ringtonev2.ui.theme.Black
+import com.example.ringtonev2.ui.theme.White
 
+@Composable
+fun PlaylistScreen(
+    onOpenPlayer: (String) -> Unit
+) {
+    val viewModel: PlaylistScreenViewModel = hiltViewModel()
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val currentPlayingId by viewModel.currentPlayingId.collectAsStateWithLifecycle()
+    val isPlaying by viewModel.isPlaying.collectAsStateWithLifecycle()
+    val favoriteIds by viewModel.favoriteIds.collectAsStateWithLifecycle()
+
+    val isEmpty = when (uiState.selectedTab) {
+        PlaylistTab.Downloads -> uiState.downloads.isEmpty()
+        PlaylistTab.Favorites -> uiState.favorites.isEmpty()
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.Black))
+            .padding(horizontal = 16.dp)
+    ) {
+        PlaylistTabRow(
+            selectedTab = uiState.selectedTab,
+            onTabSelected = viewModel::selectTab
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        when {
+            uiState.isLoading -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            isEmpty -> {
+                PlaylistEmptyState(
+                    selectedTab = uiState.selectedTab,
+                    onDownloadAudioClick = {}
+                )
+            }
+
+            else -> {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    when (uiState.selectedTab) {
+                        PlaylistTab.Downloads -> {
+                            items(
+                                items = uiState.downloads,
+                                key = { it.id }
+                            ) { download ->
+
+                                val ringtoneId = download.ringtoneId
+
+                                RingtoneItemRow(
+                                    ringtone = download.toRingtone(),
+                                    onPlayClick = {
+                                        viewModel.togglePlaying(ringtoneId)
+                                    },
+                                    isPlaying = currentPlayingId == ringtoneId && isPlaying,
+                                    onSetClick = {
+
+                                    },
+                                    onFavorite = {
+                                        viewModel.toggleFavorite(download.toRingtone())
+                                    },
+                                    isFavorite = ringtoneId in favoriteIds
+                                )
+
+                                HorizontalDivider(
+                                    modifier = Modifier.padding(vertical = 4.dp),
+                                    thickness = 0.5.dp,
+                                    color = colorResource(id = R.color.border_subtlest)
+                                )
+                            }
+                        }
+
+                        PlaylistTab.Favorites -> {
+                            items(
+                                items = uiState.favorites,
+                                key = { it.id }
+                            ) { ringtone ->
+
+                                val ringtoneId = ringtone.id.toString()
+
+                                RingtoneItemRow(
+                                    ringtone = ringtone,
+                                    onPlayClick = {
+                                        viewModel.togglePlaying(ringtoneId)
+                                    },
+                                    isPlaying = currentPlayingId == ringtoneId && isPlaying,
+                                    onSetClick = {
+                                        onOpenPlayer(ringtoneId)
+                                    },
+                                    onFavorite = {
+                                        viewModel.toggleFavorite(ringtone)
+                                    },
+                                    isFavorite = ringtoneId in favoriteIds
+                                )
+
+                                HorizontalDivider(
+                                    modifier = Modifier.padding(vertical = 4.dp),
+                                    thickness = 0.5.dp,
+                                    color = colorResource(id = R.color.border_subtlest)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PlaylistTabRow(
+    selectedTab: PlaylistTab,
+    onTabSelected: (PlaylistTab) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 12.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        PlaylistTabButton(
+            text = stringResource(R.string.downloads_tab),
+            selected = selectedTab == PlaylistTab.Downloads,
+            onClick = {
+                onTabSelected(PlaylistTab.Downloads)
+            }
+        )
+
+        PlaylistTabButton(
+            text = stringResource(R.string.favorites_tab),
+            selected = selectedTab == PlaylistTab.Favorites,
+            onClick = {
+                onTabSelected(PlaylistTab.Favorites)
+            }
+        )
+    }
+}
+
+@Composable
+private fun PlaylistTabButton(
+    text: String,
+    selected: Boolean,
+    onClick: () -> Unit
+) {
+    val backgroundColor = if (selected) {
+        colorResource(R.color.content_brand)
+    } else {
+        White.copy(0.12f)
+    }
+
+    val textColor = if (selected) {
+        colorResource(R.color.content_onbrand)
+    } else {
+        colorResource(R.color.content_subtlest)
+    }
+
+    Surface(
+        modifier = Modifier
+            .height(32.dp)
+            .clip(RoundedCornerShape(50))
+            .clickable(
+                onClick = onClick
+            ),
+        color = backgroundColor,
+        shape = RoundedCornerShape(50)
+    ) {
+        Box(
+            modifier = Modifier.padding(horizontal = 18.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text,
+                style = AppTypography.labelMedium.copy(
+                    color = textColor,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.W600
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun PlaylistEmptyState(
+    selectedTab: PlaylistTab,
+    onDownloadAudioClick: () -> Unit
+) {
+    val buttonEmpty = when (selectedTab) {
+        PlaylistTab.Downloads -> stringResource(R.string.download_tab_button)
+        PlaylistTab.Favorites -> stringResource(R.string.favorite_tab_button)
+    }
+
+    val description = when (selectedTab) {
+        PlaylistTab.Downloads -> stringResource(R.string.download_tab_description)
+        PlaylistTab.Favorites -> stringResource(R.string.favorite_tab_description)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 96.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.bg_empty),
+            contentDescription = null,
+            modifier = Modifier.size(132.dp)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = stringResource(R.string.empty_title),
+            style = AppTypography.headlineMedium.copy(
+                color = colorResource(R.color.content_default),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.W600
+            )
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = description,
+            modifier = Modifier.padding(horizontal = 32.dp),
+            textAlign = TextAlign.Center,
+            style = AppTypography.bodyLarge.copy(
+                color = colorResource(R.color.content_subtlest),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.W500
+            )
+        )
+
+
+            Spacer(modifier = Modifier.height(28.dp))
+
+            Box(
+                modifier = Modifier
+                    .height(48.dp)
+                    .width(232.dp)
+                    .clip(RoundedCornerShape(50))
+                    .background(colorResource(R.color.background_secondary))
+                    .clickable(onClick = onDownloadAudioClick),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = buttonEmpty,
+                    style = AppTypography.labelLarge.copy(
+                        color = colorResource(R.color.content_onbrand),
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.W600
+                    )
+                )
+
+        }
+    }
 }
