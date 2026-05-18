ab33c882f56cac17c2066bf9293976ac2db6468c
LeeNgocQuyet <159886685+LeeNgocQuyet@users.noreply.github.com>
2026-05-18 13:55:19 +0700

    refactor: rename database entities, DAOs, and provider methods for clarity

--- app/src/main/java/com/example/ringtonev2/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/ringtonev2/data/local/AppDatabase.kt
@@ -3,28 +3,28 @@ package com.example.ringtonev2.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.ringtonev2.data.local.dao.DownloadTiktokDao
+import com.example.ringtonev2.data.local.dao.TikTokDownloadDao
 import com.example.ringtonev2.data.local.dao.FavoriteDao
-import com.example.ringtonev2.data.local.dao.DownloadRingtoneDao
-import com.example.ringtonev2.data.local.entity.DownloadedRingtone
+import com.example.ringtonev2.data.local.dao.DownloadedRingtoneDao
+import com.example.ringtonev2.data.local.entity.TikTokDownloadEntity
 import com.example.ringtonev2.data.local.entity.FavoriteEntity
-import com.example.ringtonev2.data.local.entity.RingtoneEntity
+import com.example.ringtonev2.data.local.entity.DownloadedRingtoneEntity
 
 
 
 @Database(
     entities = [
-        DownloadedRingtone::class,
+        TikTokDownloadEntity::class,
         FavoriteEntity::class,
-        RingtoneEntity::class
+        DownloadedRingtoneEntity::class
     ],
     version = 1,
     exportSchema = false,
 )
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun downloadDao(): DownloadTiktokDao
+    abstract fun tikTokDownloadDao(): TikTokDownloadDao
     abstract fun favoriteDao(): FavoriteDao
-    abstract fun ringtoneDao(): DownloadRingtoneDao
+    abstract fun downloadedRingtoneDao(): DownloadedRingtoneDao
     companion object {
         const val NAME = "ringtone.db"
     }
